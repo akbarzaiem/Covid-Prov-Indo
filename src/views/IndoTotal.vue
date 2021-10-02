fe0b7b47a4cc57f77ffdc0a7ab4c1fe0be39e545
akbarzaiem <akbarzaiemp@gmail.com>
2021-10-02 15:13:02 +0700

    get data nasional

--- src/views/IndoTotal.vue
+++ src/views/IndoTotal.vue
@@ -1,13 +1,64 @@
 <template>
   <div class="container">
+    <!-- font graduate -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Graduate&display=swap"
+      rel="stylesheet"
+    />
+    <!-- font poppins -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap"
+      rel="stylesheet"
+    />
     <Header />
-    <div class="total">
-      <p>Positif {{ total.positif }}</p>
-      <p>Meninggal {{ total.meninggal }}</p>
-      <p>Sembuh {{ total.sembuh }}</p>
-      <p>Dirawat {{ total.dirawat }}</p>
-      <p>Update {{ total.lastUpdate }}</p>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/indototal">Data Nasional</router-link>|
+      <router-link to="/about">Data Provinsi</router-link>
     </div>
+    <div class="hasil">
+      <p class="top-tittle">Jumlah Kasus Covid 19 di Indonesia</p>
+      <div class="row" v-if="total.length !== 0">
+        <div class="col-lg-6" style="margin-bottom: 30px">
+          <p class="tittle">Positif</p>
+          <span class="data">{{ total.positif }}</span>
+          <span> terkonfirmasi</span>
+        </div>
+        <div class="col-lg-6" style="margin-bottom: 30px">
+          <p class="tittle">Sembuh</p>
+          <span class="data">{{ total.sembuh }}</span>
+          <span> jiwa</span>
+        </div>
+        <div class="col-lg-6" style="margin-bottom: 30px">
+          <p class="tittle">Dirawat</p>
+          <span class="data">{{ total.dirawat }}</span>
+          <span> jiwa</span>
+        </div>
+        <div class="col-lg-6" style="margin-bottom: 30px">
+          <p class="tittle">Meninggal</p>
+          <span class="data">{{ total.meninggal }}</span>
+          <span> jiwa</span>
+        </div>
+
+        <div class="col-lg-12">
+          <div class="update">
+            <p class="tittle">Update data</p>
+            <p>{{ total.lastUpdate }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="row" v-else>
+        <div class="col-lg-12">
+          <div class="loader mx-auto d-block"></div>
+          <p style="margin-top: 10px">Loading....</p>
+        </div>
+      </div>
+    </div>
+
     <Footer />
   </div>
 </template>
@@ -32,3 +83,41 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.hasil {
+  margin-top: 40px;
+}
+.data {
+  font-size: 50px;
+  font-family: "Graduate", cursive;
+}
+.tittle {
+  font-size: 20px;
+  font-family: "Poppins", sans-serif;
+}
+.update {
+  font-size: 13px;
+}
+.top-tittle {
+  font-size: 30px;
+  margin-bottom: 30px;
+}
+.loader {
+  border: 15px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 80px;
+  height: 80px;
+  -webkit-animation: spin 2s linear infinite; /* Safari */
+  animation: spin 2s linear infinite;
+}
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
